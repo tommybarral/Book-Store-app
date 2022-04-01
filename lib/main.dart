18a822f8c25b1b827436ae77040a7a8fdb4b9ea6
Tommybarral <isamutom27@hotmail.com>
2022-04-01 10:58:21 +0200

    starting project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:book_store_app/screens/book_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BookScreen(),
+    );
+  }
+}
---
+++ lib/screens/book_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BookScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Book Store'),
+      ),
+    );
+  }
+}
