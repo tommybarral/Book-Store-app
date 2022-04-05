2c61293da9244cd50fb744febe83551c148e0359
Tommybarral <isamutom27@hotmail.com>
2022-04-05 11:56:41 +0200

    finalizing project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:book_store_app/screens/book_details_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './screens/book_screen.dart';
@@ -17,10 +18,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: BookScreen(),
+      routes: {
+        //BookScreen.routeName: (context) => BookScreen(),
+        BookDetailsScreen.routeName: (context) => BookDetailsScreen(),
+      },
     );
   }
 }
--- lib/screens/book_screen.dart
+++ lib/screens/book_screen.dart
@@ -1,174 +1,194 @@
+import 'package:book_store_app/models/book_data.dart';
+import 'package:book_store_app/screens/book_details_screen.dart';
+//import 'package:book_store_app/widgets/custom_tab.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class BookScreen extends StatelessWidget {
+
+  static const routeName = '/book-screen';
 
   var _searchBook = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        elevation: 5,
-        color: Colors.white,
-        child: Container(
-          height: 62,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Icon(Icons.home),
-              Icon(Icons.favorite_border),
-              Icon(Icons.person_rounded),
-            ],
-          ),
-        ),
-      ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: Colors.white60,
+      body: SafeArea(
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Hi tommy'),
-                Text('Discover Lastest Book'),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(0.7),
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: TextField(
-                        controller: _searchBook,
-                        autofocus: false,
-                        autocorrect: false,
-                        enableSuggestions: false,
-                        obscureText: false,
-                        decoration: InputDecoration.collapsed(
-                          hintText: 'Search book',
-                        ),
+          child: ListView(
+            padding: const EdgeInsets.only(left: 10.0, top: 15.0),
+            physics: BouncingScrollPhysics(),
+            children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Hi tommy', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),),
+                    Text('Discover Lastest Book', style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),),
+                    Container(
+                      height: 45,
+                      margin: EdgeInsets.only(top: 18, left: 25, right: 25),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(7),
+                      ),
+                      child: Stack(
+                        children: [
+                          TextField(
+                            maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                            controller: _searchBook,
+                            autofocus: false,
+                            autocorrect: false,
+                            enableSuggestions: false,
+                            obscureText: false,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 19, right: 50, bottom: 8),
+                              border: InputBorder.none,
+                              hintText: 'Search book ...',
+                            ),
+                          ),
+                          Positioned(
+                            right: 0,
+                            child: Container(
+                                  height: 45,
+                                  width: 45,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: Colors.red.withOpacity(0.7),
+                                  ),
+                                child: IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.search, color: Colors.white,),),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('New'),
-                    Text('Trending'),
-                    Text('Best seller'),
-                    SizedBox(
-                      width: 150,
+                    Container(
+                      height: 25,
+                      margin: EdgeInsets.only(top: 30),
+                      padding: EdgeInsets.only(left: 25),
+                      child: DefaultTabController(
+                          length: 3,
+                          child: TabBar(
+                            labelPadding: EdgeInsets.all(0),
+                            indicatorPadding: EdgeInsets.all(0),
+                            isScrollable: true,
+                            labelColor: Colors.black,
+                            unselectedLabelColor: Colors.grey,
+                            labelStyle: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            unselectedLabelStyle: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w700
+                            ),
+                            tabs: [
+                              Tab(
+                                child: Container(
+                                  margin: EdgeInsets.only(right: 23),
+                                  child: Text('New'),
+                                ),
+                              ),
+                              Tab(
+                                child: Container(
+                                  margin: EdgeInsets.only(right: 23),
+                                  child: Text('Trending'),
+                                ),
+                              ),
+                              Tab(
+                                child: Container(
+                                  margin: EdgeInsets.only(right: 23),
+                                  child: Text('Best seller'),
+                                ),
+                              ),
+                            ],
+                          ),
+                      ),
                     ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 225,
-                    width: double.infinity,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 0, top: 8.0, right: 16.0),
-                          child: Container(
-                            height: 110,
-                            width: 165,
+                    Container(
+                      margin: EdgeInsets.only(top: 21),
+                      height: 210,
+                      child: ListView.builder(
+                        padding: EdgeInsets.only(left: 25, right: 6),
+                        physics: BouncingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 3,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            height: 210,
+                            width: 153,
+                            margin: EdgeInsets.only(right: 19),
                             decoration: BoxDecoration(
-                              color: Colors.grey,
                               borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Center(child: Text('Book here')),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 0, top: 8.0, right: 16.0),
-                          child: Container(
-                            height: 110,
-                            width: 165,
-                            decoration: BoxDecoration(
                               color: Colors.grey,
-                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: NetworkImage('https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1637008457'),
+                                fit: BoxFit.fill,
+                              ),
                             ),
-                            child: Center(child: Text('Book here')),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 0, top: 8.0, right: 16.0),
-                          child: Container(
-                            height: 110,
-                            width: 165,
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Center(child: Text('Book here')),
-                          ),
-                        ),
-                      ],
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                ),
-                Row(
-                  children: [
-                    Text('Popular'),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 150,
-                    width: double.infinity,
-                    child: ListView(
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25.0, top: 25.0),
+                      child: Text(
+                        'Popular',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    ListView.builder(
+                      padding: EdgeInsets.only(top: 25, right: 25, left: 25),
+                      physics: BouncingScrollPhysics(),
+                      shrinkWrap: true,
                       scrollDirection: Axis.vertical,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 0, top: 8.0, bottom: 8.0),
+                      itemCount: 3,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pushNamed(BookDetailsScreen.routeName);
+                          },
                           child: Container(
-                            height: 75,
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(10),
+                            height: 81,
+                            width: MediaQuery.of(context).size.width - 50,
+                            margin: EdgeInsets.only(bottom: 19),
+                            color: Colors.grey.withOpacity(0.4),
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 81,
+                                  width: 62,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    image: DecorationImage(
+                                        image: NetworkImage('https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1637008457')),
+                                  ),
+                                ),
+                                SizedBox(width: 21),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('You\'re a miracle', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),),
+                                    SizedBox(height: 5),
+                                    Text('Author', style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400),),
+                                    SizedBox(height: 5),
+                                    Text('20', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),),
+                                  ],
+                                ),
+                              ],
                             ),
-                            child: Center(child: Text('Book here')),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 0, top: 8.0, bottom: 8.0),
-                          child: Container(
-                            height: 75,
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Center(child: Text('Book here')),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 0, top: 8.0, bottom: 8.0),
-                          child: Container(
-                            height: 75,
-                            decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Center(child: Text('Book here')),
-                          ),
-                        ),
-                      ],
+                        );
+                      },
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+            ],
           ),
         ),
       ),
